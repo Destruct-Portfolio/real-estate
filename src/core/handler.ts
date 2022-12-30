b3d859a7f82df30ecb3da00a1804c0048a8e8aa4
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2022-12-30 16:28:34 +0100

    ..

--- src/core/handler.ts
+++ src/core/handler.ts
@@ -1,19 +1,22 @@
 import halou_updated from "../componants/halooglasi_updated.js"
 import Nekretinine_updated from "../componants/nekretinine_updated.js"
-import Zida_updated from "../componants/zida_updated.js"
 import sas_updated from "../componants/sas_updated.js"
+import { Ad_Object } from "src/types/index.js"
+import Zida from "../componants/zida2.js"
 
 export default class Handler {
   private static scrapers = [
-    /*    halou_updated,
+    halou_updated,
     sas_updated,
-    Nekretinine_updated, */
-    Zida_updated
+    Nekretinine_updated,
+    Zida
   ]
 
   public static async exec() {
+    let results: Ad_Object[][] = []
     for (const scraper of Handler.scrapers) {
-      await new scraper().exec()
+      results.push(await new scraper().exec())
     }
+    return results
   }
 }

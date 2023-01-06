10a042a90cfcd5e216cc9971b873a96dea081e2e
Ben Hichem <77390862+benhichem@users.noreply.github.com>
2023-01-06 22:06:16 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,11 @@ import scheduale from "node-schedule"
 
 await Handler.exec()
 
-console.log('Lunching the Second Time')
-
-//await Handler.exec()
-/* const TimeConfig = new scheduale.RecurrenceRule();
+const TimeConfig = new scheduale.RecurrenceRule();
 TimeConfig.hour = [0, 10, 20, 30, 40, 50];
 TimeConfig.dayOfWeek = [0, new scheduale.Range(1, 6)];
 
 scheduale.scheduleJob(TimeConfig, async function () {
   // Your code to run the job goes here
   await Handler.exec();
-}); */
+}); 
